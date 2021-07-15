db8581b09d2998d01c5542dd28f2f11181d525e7
mudassir <mudassirwaseem23@gmail.com>
2021-07-15 22:18:32 +0500

    some changes in branc 3

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -8,6 +8,7 @@ const useFetch = ({ url, category }) => {
       const rawData = await fetch(url);
       const dataAPI = await rawData.json();
       console.log(dataAPI, "api");
+      console.log(dataAPI, "api");
       setData(
         dataAPI.entries.filter((element) => element.programType == category)
       );
