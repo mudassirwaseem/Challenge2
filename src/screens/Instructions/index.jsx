fd02f0ad270c7cbe554b108f7262a766df7670c1
mudassirwaseem <mudassirwaseem24@gmail.com>
2022-08-11 15:26:10 +0500

    change in headiing fot test netlify

--- src/screens/Instructions/index.jsx
+++ src/screens/Instructions/index.jsx
@@ -27,7 +27,7 @@ const Instructions = () => {
   return (
     <div className={classes.root}>
       <Typography className={classes.title} variant={"h4"}>
-        Geeks of Kolachi
+        Instructions
       </Typography>
       <div>
         <Typography variant={"h5"}>Note</Typography>
